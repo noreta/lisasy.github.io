58c57887e7e4247c6cbf371484eff288e8e8af30
Lisa Sy <lisasyis@gmail.com>
2014-09-09 23:14:38 -0700

    Fix a lot of conflicts in merging

--- js/article-0aaafbf1.js
+++
@@ -1,8 +0,0 @@
-(function() {
-  jQuery(function($) {
-    return $('.article-body p').html(function(i, html) {
-      return html.replace(/(\w+\s\w+\s\w+\s\w+\s\w+)/, '<span class="intro">$1</span>');
-    });
-  });
-
-}).call(this);
